f5ef4f3a57d66869581fbd69d0955c94cea6fed3
Seydlitz-code <153933205+Seydlitz-code@users.noreply.github.com>
2024-03-17 23:10:43 +0900

    Lab_01_03

--- Code.cs
+++ Code.cs
@@ -6,18 +6,10 @@ public class Code : MonoBehaviour
 {
     private void Awake()
     {
-        float floatValue01 = 3.141592653589793238462643383279f;
-        //7자리 표현
-        float floatValue02 = 31.41592653589793238462643383279f;
-        //7자리 표현
-        double doubleValue = 3.141592653589793238462643383279;
-        //15자리 표현
-        decimal decimalValue = 3.141592653589793238462643383279m;
-        //29자리 표현
+        string stringValue = "안녕하세요. 이동환입니다.";
+        bool boolValue = true;
 
-        Debug.Log("float Data : " + floatValue01);
-		Debug.Log("float Data : " + floatValue02);
-		Debug.Log("double Data : " + doubleValue);
-		Debug.Log("decimal : " + decimalValue);
-	}
+        Debug.Log("string Data : " + stringValue);
+        Debug.Log("bool Data : " +  boolValue);
+    }
 }
