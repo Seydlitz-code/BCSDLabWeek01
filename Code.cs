2fc8ea4064c24eee28f58f95f22682f3cf5c1757
Seydlitz-code <153933205+Seydlitz-code@users.noreply.github.com>
2024-03-17 23:11:39 +0900

    Lab_01_05

--- Code.cs
+++ Code.cs
@@ -6,16 +6,17 @@ public class Code : MonoBehaviour
 {
     private void Awake()
     {
-        object valueInt = 31;
-        //빅싱 (데이터 저장)
-        object valueFloat = 3.14159265358979f;
-        //빅싱 (데이터 저장)
-        object valueString = "객체지향 프로그래밍";
-        object valueBool = false;
+        sbyte sbyteValue = 10;
+        int intValue = (int)sbyteValue;
+        Debug.Log("sbyteValue : " + sbyteValue);
+        Debug.Log("intValue : " + intValue);
 
-        Debug.Log("정수 : " + valueInt);
-		Debug.Log("실수 : " + valueFloat);
-		Debug.Log("문자열 : " + valueString);
-		Debug.Log("논리 : " + valueBool);
+        intValue = 130;
+		//int   -2147483648 ~ 2147483647;
+        //sbyte   -128 ~ 127
+		sbyteValue = (sbyte)intValue;
+		//sbyte의 최대 범위인 127보다 큰 130을 전달했기 때문에 오버플로우 발생
+		Debug.Log("sbyteValue : " + sbyteValue);
+		Debug.Log("intValue : " + intValue);
 	}
 }
