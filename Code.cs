0c4be2b95717f2bab3623f166ea1976494df59ef
Seydlitz-code <153933205+Seydlitz-code@users.noreply.github.com>
2024-03-17 23:12:44 +0900

    Lab_01_07

--- Code.cs
+++ Code.cs
@@ -4,23 +4,18 @@ using UnityEngine;
 
 public class Code : MonoBehaviour
 {
-	private void Awake()
-	{
-		sbyte sbyteValue = 31;
-		byte byteValue = (byte)sbyteValue;
-		Debug.Log("sbyteValue : " + sbyteValue);
-		Debug.Log("byteValue : " + byteValue);
+    private void Awake()
+    {
+        float floatValue = 69.6875f;
+        double doubleValue = (double)floatValue;
 
-		// 부호 있는 정수가 음수일 때
-		sbyteValue = -31;
-		byteValue = (byte)sbyteValue;
-		Debug.Log("sbyteValue : " + sbyteValue);
-		Debug.Log("byteValue : " + byteValue);
+        Debug.Log("floatValue : " + floatValue);
+        Debug.Log("doubleValue : " + doubleValue);
 
-		// 부호 있는 정수가 부호 없는 정수의 최대값보다 클 때
-		byteValue = 200;
-		sbyteValue = (sbyte)byteValue;
-		Debug.Log("sbyteValue : " + sbyteValue);
-		Debug.Log("byteValue : " + byteValue);
+        floatValue = 0.1f;
+        doubleValue = (double)floatValue;
+
+		Debug.Log("floatValue : " + floatValue);
+		Debug.Log("doubleValue : " + doubleValue);
 	}
 }
