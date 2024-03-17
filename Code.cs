4c039b91b05d4fe19fa00043c6737f0344da074c
Seydlitz-code <153933205+Seydlitz-code@users.noreply.github.com>
2024-03-17 23:12:03 +0900

    Lab_01_06

--- Code.cs
+++ Code.cs
@@ -4,19 +4,23 @@ using UnityEngine;
 
 public class Code : MonoBehaviour
 {
-    private void Awake()
-    {
-        sbyte sbyteValue = 10;
-        int intValue = (int)sbyteValue;
-        Debug.Log("sbyteValue : " + sbyteValue);
-        Debug.Log("intValue : " + intValue);
-
-        intValue = 130;
-		//int   -2147483648 ~ 2147483647;
-        //sbyte   -128 ~ 127
-		sbyteValue = (sbyte)intValue;
-		//sbyte의 최대 범위인 127보다 큰 130을 전달했기 때문에 오버플로우 발생
+	private void Awake()
+	{
+		sbyte sbyteValue = 31;
+		byte byteValue = (byte)sbyteValue;
 		Debug.Log("sbyteValue : " + sbyteValue);
-		Debug.Log("intValue : " + intValue);
+		Debug.Log("byteValue : " + byteValue);
+
+		// 부호 있는 정수가 음수일 때
+		sbyteValue = -31;
+		byteValue = (byte)sbyteValue;
+		Debug.Log("sbyteValue : " + sbyteValue);
+		Debug.Log("byteValue : " + byteValue);
+
+		// 부호 있는 정수가 부호 없는 정수의 최대값보다 클 때
+		byteValue = 200;
+		sbyteValue = (sbyte)byteValue;
+		Debug.Log("sbyteValue : " + sbyteValue);
+		Debug.Log("byteValue : " + byteValue);
 	}
 }
