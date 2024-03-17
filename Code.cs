97cd5508d68cdd8379f3760c42a9f1ebca30fdb8
Seydlitz-code <153933205+Seydlitz-code@users.noreply.github.com>
2024-03-17 23:13:52 +0900

    Lab_01_10

--- Code.cs
+++ Code.cs
@@ -6,22 +6,17 @@ public class Code : MonoBehaviour
 {
     private void Awake()
     {
-        //문자열(string)에 숫자가 아닌 데이터가 들어있을 경우 숫자로 형 변환을 시도하면 에러가 발생한다.
+        string stringValue = "숫자가 아닙니다.";
         int intValue = 10;
-        float floatValue = 12.3456f;
-        string stringValue = "33";
-
-        Debug.Log("intValue : " + intValue);
-		Debug.Log("floatValue : " + floatValue);
-		Debug.Log("stringValue : " + stringValue);
-
-        //33.4567과 같은 실수를 int로 형변환을 시도하면 에러 발생
-        intValue = int.Parse(stringValue);
-        stringValue = "33.4567";
-        floatValue = float.Parse(stringValue);
-
-		Debug.Log("intValue : " + intValue);
-		Debug.Log("floatValue : " + floatValue);
-		Debug.Log("stringValue : " + stringValue);
-	}
+		bool isConversion = int.TryParse(stringValue, out intValue);
+		if (isConversion == true)
+        {
+            Debug.Log("stringValue : " + stringValue);
+			Debug.Log("intValue : " + intValue);
+		}
+        else
+        {
+			Debug.Log("stringValue 변수에 들어있는 내용이 숫자가 아니어서 형 변환에 실패했습니다.");
+		}
+    }
 }
