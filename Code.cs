bd89c23cf4b7ef5283da48ca4bbabc4c37db43ce
Seydlitz-code <153933205+Seydlitz-code@users.noreply.github.com>
2024-03-17 23:14:19 +0900

    Lab_01_11

--- Code.cs
+++ Code.cs
@@ -6,17 +6,17 @@ public class Code : MonoBehaviour
 {
     private void Awake()
     {
-        string stringValue = "숫자가 아닙니다.";
-        int intValue = 10;
-		bool isConversion = int.TryParse(stringValue, out intValue);
-		if (isConversion == true)
-        {
-            Debug.Log("stringValue : " + stringValue);
-			Debug.Log("intValue : " + intValue);
-		}
-        else
-        {
-			Debug.Log("stringValue 변수에 들어있는 내용이 숫자가 아니어서 형 변환에 실패했습니다.");
-		}
-    }
+        string stringValue = "초기화";
+        int intValue = 33;
+        float floatValue = 12.3456f;
+        Debug.Log("stringValue : " + stringValue);
+
+        //정수를 문자열로 형 변환
+        stringValue = intValue.ToString();
+		Debug.Log("stringValue : " + stringValue);
+
+        //실수를 문자열로 형 변환
+        stringValue = floatValue.ToString();
+		Debug.Log("stringValue : " + stringValue);
+	}
 }
